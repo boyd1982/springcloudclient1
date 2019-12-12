3807a8dd1e43f15c88478c37fcb3e0769f340813
boyd1982 <xubosuper@163.com>
2019-12-12 15:46:33 +0800

    Initial commit

---
+++ src/main/java/com/example/springcloudclient1/Springcloudclient1Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloudclient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloudclient1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudclient1/Springcloudclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloudclient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
